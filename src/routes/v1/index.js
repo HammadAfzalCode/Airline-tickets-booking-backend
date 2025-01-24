1ca168caab742fdbd5bf56d77c28a9a6023c58f5
Hammad Afzal <hammad.afzal.code@gmail.com>
2025-01-24 20:27:51 +0500

    added basic express routing

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/info", (req, res) => {
+  return res.json({ msg: "ok" });
+});
+
+module.exports = router;
